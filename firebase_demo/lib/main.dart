e4adce1ebe74b8daa5a1b4001705517bfb0e3e82
Santhoji07 <santhoji07msd@gmail.com>
2025-06-17 11:02:42 +0530

    demo_firbase

---
+++ firebase_demo/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_demo/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main()async{
+  const Firebase.initializeApp(options: FirebaseOptions(apiKey: "AIzaSyDMf4kyQ3_X9PF5FqLpvBrnNUfHoRRnC2o",
+  authDomain: "fir-733ff.firebaseapp.com",
+  projectId: "fir-733ff",
+  storageBucket: "fir-733ff.firebasestorage.app",
+  messagingSenderId: "988477366143",
+  appId: "1:988477366143:web:370264a9c0ab11f091044a"));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
